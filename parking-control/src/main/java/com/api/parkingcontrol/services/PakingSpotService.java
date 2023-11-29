50474892161f037c6379222e510526c2187027f1
simone-art <simoneperiodista3@gmail.com>
2023-11-29 17:28:16 -0300

    Injecão de dependência do repository via constructor

--- parking-control/src/main/java/com/api/parkingcontrol/services/PakingSpotService.java
+++ parking-control/src/main/java/com/api/parkingcontrol/services/PakingSpotService.java
@@ -1,12 +1,15 @@
 package com.api.parkingcontrol.services;
 
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PakingSpotService {
 
-    @Autowired
-    private ParkingSpotRepository parkingSpotRepository;
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public PakingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
 }
