ca9403fb106971ae3d2489f98d8aa64fea92e4b5
simone-art <simoneperiodista3@gmail.com>
2023-11-30 14:22:08 -0300

    Inclusão dependência MySql e ajustes nos properties

--- parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
+++ parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -6,16 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ParkingControlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingControlApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String index(){
-		return "Olá Mundo!";
-	}
+//	@GetMapping("/")
+//	public String index(){
+//		return "Olá Mundo!";
+//	}
 
 }
